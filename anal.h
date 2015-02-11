625d1d711a5efe957613a5fcf199afcd21af5050
aalbrechtsen <andersdetermig@gmail.com>
2015-02-11 14:21:36 +0100

    made output understandable and ignores betas for covariates

--- anal.h
+++ anal.h
@@ -5,7 +5,7 @@
 #include "readplink.h"
 #include "kstring.h"
 
-void wrap(const plink *p,const std::vector<double> &phe,const std::vector<double> &ad,Matrix<double> &freq,int model,std::vector<double> start,Matrix<double> &cov,int,double,std::vector<char*> &loci,int nThreads);
+void wrap(const plink *p,const std::vector<double> &phe,const std::vector<double> &ad,Matrix<double> &freq,int model,std::vector<double> start,Matrix<double> &cov,int,double,std::vector<char*> &loci,int nThreads,FILE *outFile);
 
 /*
   Below is the pars.
